0de2a29d2fae7dd84469c37088395bda5b21c26e
abyasa05 <abyprat07@gmail.com>
2025-02-27 23:09:47 +0700

    break down & specify car interfaces

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/CarAccess.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarAccess.java
@@ -2,10 +2,13 @@ package id.ac.ui.cs.advprog.eshop.service;
 import id.ac.ui.cs.advprog.eshop.model.Car;
 import java.util.List;
 
-public interface CarService {
-    public Car create(Car car);
+public interface CarAccess {
     public List<Car> findAll();
-    Car findById(String carId);
+    public Car findById(String carId);
+}
+
+interface CarUpdate {
+    public Car create(Car car);
     public void update(String carId, Car car);
     public void deleteCarById(String carId);
 }
--- src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
@@ -8,7 +8,7 @@ import java.util.Iterator;
 import java.util.List;
 
 @Service
-public class CarServiceImpl implements CarService {
+public class CarServiceImpl implements CarAccess, CarUpdate {
     @Autowired
     private CarRepository carRepository;
 
