1367ed01b3f2e54331e8aff56287e3c7073d556a
abyasa05 <abyprat07@gmail.com>
2025-02-28 01:47:12 +0700

    refactor CarRepository interface & impl

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
@@ -1,5 +1,4 @@
 package id.ac.ui.cs.advprog.eshop.repository;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import id.ac.ui.cs.advprog.eshop.model.Car;
 import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
@@ -7,20 +6,31 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
 
-interface IdGeneration {
-    public String generateId();
-}
-
-interface UpdateCarRepository {
+public interface CarRepository {
+    public Car create(Car car);
+    public Iterator<Car> findAll();
+    public Car findById(String id);
     public void update(String id, Car car);
     public void delete(String id);
 }
 
-@Repository
-public class CarRepository implements UpdateCarRepository {
-    private List<Car> carData = new ArrayList<>();
-    private IdGenerator idGenerator = new IdGenerator();
+interface IdGeneration {
+    public String generateId();
+}
 
+class IdGenerator implements IdGeneration {
+    @Override
+    public String generateId() {
+        return UUID.randomUUID().toString();
+    }
+}
+
+@Repository
+class CarRepositoryImpl implements CarRepository {
+    private final List<Car> carData = new ArrayList<>();
+    private final IdGenerator idGenerator = new IdGenerator();
+
+    @Override
     public Car create(Car car) {
         if (car.getCarId() == null) {
             car.setCarId(idGenerator.generateId());
@@ -29,10 +39,12 @@ public class CarRepository implements UpdateCarRepository {
         return car;
     }
 
+    @Override
     public Iterator<Car> findAll() {
         return carData.iterator();
     }
 
+    @Override
     public Car findById(String id) {
         for (Car car : carData) {
             if (car.getCarId().equals(id)) {
@@ -58,12 +70,5 @@ public class CarRepository implements UpdateCarRepository {
     @Override
     public void delete(String id) {
         carData.removeIf(car -> car.getCarId().equals(id));
-    }
-}
-
-class IdGenerator implements IdGeneration {
-    @Override
-    public String generateId() {
-        return UUID.randomUUID().toString();
     }
 }
